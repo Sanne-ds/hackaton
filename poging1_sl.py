b8a100e12137af30b4d9625a8f01662c0815ea5d
Sanne-ds <sanne.groenland@hva.nl>
2025-03-26 14:24:23 +0100

    Update poging1_sl.py

--- poging1_sl.py
+++ poging1_sl.py
@@ -1,25 +1,18 @@
 import plotly.express as px
 import pandas as pd
+import requests
+import numpy as np
 
-@st.cache_data
-def fetch_data():
-    url = 'https://sensornet.nl/dataserver3/event/collection/nina_events/stream?conditions%5B0%5D%5B%5D=time&conditions%5B0%5D%5B%5D=%3E%3D&conditions%5B0%5D%5B%5D=1735689600&conditions%5B1%5D%5B%5D=time&conditions%5B1%5D%5B%5D=%3C&conditions%5B1%5D%5B%5D=1742774400&conditions%5B2%5D%5B%5D=label&conditions%5B2%5D%5B%5D=in&conditions%5B2%5D%5B%5D=21&conditions%5B2%5D%5B%5D=32&conditions%5B2%5D%5B%5D=33&conditions%5B2%5D%5B%5D=34&args%5B%5D=aalsmeer&args%5B%5D=schiphol&fields%5B%5D=time&fields%5B%5D=location_short&fields%5B%5D=location_long&fields%5B%5D=duration&fields%5B%5D=SEL&fields%5B%5D=SELd&fields%5B%5D=SELe&fields%5B%5D=SELn&fields%5B%5D=SELden&fields%5B%5D=SEL_dB&fields%5B%5D=lasmax_dB&fields%5B%5D=callsign&fields%5B%5D=type&fields%5B%5D=altitude&fields%5B%5D=distance&fields%5B%5D=winddirection&fields%5B%5D=windspeed&fields%5B%5D=label&fields%5B%5D=hex_s&fields%5B%5D=registration&fields%5B%5D=icao_type&fields%5B%5D=serial&fields%5B%5D=operator&fields%5B%5D=tags'
-    
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Zorgt ervoor dat een HTTP-fout een uitzondering veroorzaakt
-        
-        if response.status_code == 200:
-            colnames = pd.DataFrame(response.json()['metadata'])
-            data = pd.DataFrame(response.json()['rows'])
-            data.columns = colnames.headers
-            data['time'] = pd.to_datetime(data['time'], unit='s')
-            return data
-        else:
-            return None  # Als er een fout is, geef dan geen data terug
-            
-    except requests.exceptions.RequestException:
-        return None  # Als er een netwerkfout of andere fout is, geef dan ook geen data terug
+start_date = int(pd.to_datetime('2025-03-01').timestamp())
+end_date = int(pd.to_datetime('2025-03-7').timestamp())
+response = requests.get(f'https://sensornet.nl/dataserver3/event/collection/nina_events/stream?conditions%5B0%5D%5B%5D=time&conditions%5B0%5D%5B%5D=%3E%3D&conditions%5B0%5D%5B%5D={start_date}&conditions%5B1%5D%5B%5D=time&conditions%5B1%5D%5B%5D=%3C&conditions%5B1%5D%5B%5D={end_date}&conditions%5B2%5D%5B%5D=label&conditions%5B2%5D%5B%5D=in&conditions%5B2%5D%5B2%5D%5B%5D=21&conditions%5B2%5D%5B2%5D%5B%5D=32&conditions%5B2%5D%5B2%5D%5B%5D=33&conditions%5B2%5D%5B2%5D%5B%5D=34&args%5B%5D=aalsmeer&args%5B%5D=schiphol&fields%5B%5D=time&fields%5B%5D=location_short&fields%5B%5D=location_long&fields%5B%5D=duration&fields%5B%5D=SEL&fields%5B%5D=SELd&fields%5B%5D=SELe&fields%5B%5D=SELn&fields%5B%5D=SELden&fields%5B%5D=SEL_dB&fields%5B%5D=lasmax_dB&fields%5B%5D=callsign&fields%5B%5D=type&fields%5B%5D=altitude&fields%5B%5D=distance&fields%5B%5D=winddirection&fields%5B%5D=windspeed&fields%5B%5D=label&fields%5B%5D=hex_s&fields%5B%5D=registration&fields%5B%5D=icao_type&fields%5B%5D=serial&fields%5B%5D=operator&fields%5B%5D=tags')
+colnames = pd.DataFrame(response.json()['metadata'])
+data = pd.DataFrame(response.json()['rows'])
+data.columns = colnames.headers
+data['time'] = pd.to_datetime(data['time'], unit = 's')
+print(data['time'].min(),data['time'].max())
+print(data.shape)
+data.head()
 data = data.dropna(subset=['type'])
 data.head()
 
