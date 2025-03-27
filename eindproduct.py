e3009e4c950ef19bba3173bb47e7203f2fa7438f
Sanne-ds <sanne.groenland@hva.nl>
2025-03-27 15:10:09 +0100

    Update eindproduct.py

--- eindproduct.py
+++ eindproduct.py
@@ -1,16 +1,15 @@
 import streamlit as st
 import plotly.graph_objects as go
-import pandas as pd
 import plotly.express as px
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import numpy as np
 import requests
 
 # Tabbladen maken
 tab1, tab2 = st.tabs(["Fabrikanten & Boeing Modellen", "Marijn's Visualisaties"])
 
-# Tabblad 1: Jouw code
+# Tabblad 1: Jouw visualisaties
 with tab1:
     st.title("Luidste Vliegtuigfabrikanten Analyse")
 
@@ -152,10 +151,9 @@ with tab2:
     st.title("Marijn's Visualisaties")
 
     # Voeg hier de code van Marijn toe
-    # Cache de gegevensophaal functie om onnodige herhalingen van verzoeken te voorkomen
     @st.cache_data
     def fetch_data():
-        url = 'https://sensornet.nl/dataserver3/event/collection/nina_events/stream?conditions%5B0%5D%5B%5D=time&conditions%5B0%5D%5B%5D=%3E%3D&conditions%5B0%5D%5B%5D=1735689600&conditions%5B1%5D%5B%5D=time&conditions%5B1%5D%5B%5D=%3C&conditions%5B1%5D%5B%5D=1742774400&conditions%5B%5D%5B%5D=label&conditions%5B%5D%5B%5D=in&conditions%5B%5D%5B%5D=21&conditions%5B%5D%5B%5D=32&conditions%5B%5D%5B%5D=33&conditions%5B%5D%5B%5D=34&args%5B%5D=aalsmeer&args%5B%5D=schiphol&fields%5B%5D=time&fields%5B%5D=location_short&fields%5B%5D=location_long&fields%5B%5D=duration&fields%5B%5D=SEL&fields%5B%5D=SELd&fields%5B%5D=SELe&fields%5B%5D=SELn&fields%5B%5D=SELden&fields%5B%5D=SEL_dB&fields%5B%5D=lasmax_dB&fields%5B%5D=callsign&fields%5B%5D=type&fields%5B%5D=altitude&fields%5B%5D=distance&fields%5B%5D=winddirection&fields%5B%5D=windspeed&fields%5B%5D=label&fields%5B%5D=hex_s&fields%5B%5D=registration&fields%5B%5D=icao_type&fields%5B%5D=serial&fields%5B%5D=operator&fields%5B%5D=tags'
+        url = 'https://sensornet.nl/dataserver3/event/collection/nina_events/stream?...'
         response = requests.get(url)
         response.raise_for_status()
         colnames = pd.DataFrame(response.json()['metadata'])
@@ -175,5 +173,6 @@ with tab2:
         })
         return data
 
-    # Voeg hier de rest van Marijn's code toe
-    # Bijvoorbeeld: Berekeningen, visualisaties, enz.
+    # Voeg hier de rest van Marijn's visualisaties toe
+    # Bijvoorbeeld: Grafieken, berekeningen, enz.
+    
