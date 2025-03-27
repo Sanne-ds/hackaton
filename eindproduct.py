bc21a63e65bd54fcceee50d46334a1c4d1834d12
Sanne-ds <sanne.groenland@hva.nl>
2025-03-27 15:43:46 +0100

    Update eindproduct.py

--- eindproduct.py
+++ eindproduct.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Titel van de Streamlit app
-st.title("🛫Hackaton")
+st.title("Hackaton 👩🏼‍✈️👨🏻‍✈️👨🏼‍✈️🧑🏻‍✈️")
 
 # Maak twee tabbladen
 tab1, tab2, tab3, tab4 = st.tabs(["Vliegtuigfabrikanten", "Passagiers en vracht", "Geluidsoverzicht", "Geluidsdetectie"])
