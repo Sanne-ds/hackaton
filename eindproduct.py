906580fec03057e19f9daab217f8615959fe51b8
Sanne-ds <sanne.groenland@hva.nl>
2025-03-27 15:32:13 +0100

    Update eindproduct.py

--- eindproduct.py
+++ eindproduct.py
@@ -532,7 +532,3 @@ with tab3:
    
    # Toon de chart
    st.plotly_chart(fig_weekday_chart, use_container_width=True, key="weekday_chart")
-
-with tab4: 
- st.header("Zach")
-    st.write("Zach visualisaties")
